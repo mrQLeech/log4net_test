243f6eb274b1f6cddce1e168bcfac72e4cd3b336
Krasnov <krasnov@parmalogica.ru>
2016-09-19 19:34:44 +0500

    creation

---
+++ Log4Net_test/Log4Net_test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log4Net_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Testing();
+            Console.ReadLine();            
+        }
+
+        internal static void Testing()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            try
+            {
+                string str = String.Empty;
+                string subStr = str.Substring(0, 4); //this line will create error as the string "str" is empty.  
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error Message: " + ex.Message.ToString(), ex);
+            }
+        }
+    }
+}
